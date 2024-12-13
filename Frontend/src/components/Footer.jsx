72a0b1440963322f3e75b0fd9b4540fb898489df
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-12-13 14:00:33 +0545

    Home page completed

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            recusandae, error beatae impedit, adipisci tempore mollitia eum iste
+            pariatur culpa ea consectetur suscipit ex fuga tenetur enim dolore,
+            asperiores iusto.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+977-9869450879</li>
+            <li>dummyEmail@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ copyrighttoooo -All right Reserved
+        </p>
+      </div>
+    </div>
+  );
+}
