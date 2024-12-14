afb2cc89de3a9a8feff555a0713ebc88e7a87573
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-12-14 18:32:56 +0545

    userController created

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,10 @@
+// Route for user login
+const loginUser = async (req, res) => {};
+
+// Router for user register
+const registerUser = async (req, res) => {};
+
+// Router for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
