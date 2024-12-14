839548fb7ea77c60484ebe1c491fc3ff94bef027
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-12-14 09:42:32 +0545

    About section completed

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import Title from "./../components/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            We are a team of passionate individuals who are dedicated to
+            creating high-quality products that make a positive impact on our
+            customers' lives. We believe in the power of innovation and are
+            constantly looking
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi
+            tempore cupiditate culpa consequuntur facere delectus incidunt
+            beatae optio tenetur expedita, qui repellat sapiente adipisci quae
+            cumque nisi voluptatem voluptate praesentium?
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+            voluptates eligendi esse distinctio at cumque atque iste commodi
+            architecto consequuntur nemo sit magni soluta pariatur dolorum amet,
+            illum praesentium. Unde?
+          </p>
+        </div>
+      </div>
+
+      <div className="text-2xl py-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py:20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            repellendus, aliquam, excepturi quod enim quis delectus illum at id
+            dolores itaque aut,
+          </p>
+        </div>
+
+        <div className="border px-10 md:px-16 py-8 sm:py:20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            repellendus, aliquam, excepturi quod enim quis delectus illum at id
+            dolores itaque aut,
+          </p>
+        </div>
+
+        <div className="border px-10 md:px-16 py-8 sm:py:20 flex flex-col gap-5">
+          <b>Convenience</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            repellendus, aliquam, excepturi quod enim quis delectus illum at id
+            dolores itaque aut,
+          </p>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default About;
